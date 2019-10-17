080baefcf93e4101982c5b789689dec88c7be606
mickey english <mickeyeng92@gmail.com>
2019-10-17 01:34:17 +0100

    Profile component styled

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Sky from '../images/sky.png';
 
 const Profile = () => {
     return (
         <div className="profile">
-            <h3>Profile</h3>
+            <img className="profile__background" src={Sky} />
+            <div className="profile-box" >
+                <img
+                    className="profile__image"
+                    src="https://i.pravatar.cc/75"
+                />
+                <ul>
+                    <li>Tweets: </li>
+                    <li>10 </li>
+                    <li>Following: </li>
+                    <li>5 </li>
+                    <li>Followers: </li>
+                    <li>20 </li>
+                </ul>
+            </div>
+
         </div>
     );
 }
